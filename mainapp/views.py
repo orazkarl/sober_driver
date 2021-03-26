b5a48212a5949d42a7a4103da431a2061395f542
orazkarl <43617375+orazkarl@users.noreply.github.com>
2021-03-26 14:08:38 +0600

    bug fixed

--- mainapp/views.py
+++ mainapp/views.py
@@ -6,6 +6,7 @@ from django.contrib.sessions.models import Session
 from django.utils import timezone
 import datetime
 
+
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
@@ -13,6 +14,7 @@ def get_client_ip(request):
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
+
 
 class HomeView(generic.TemplateView):
     template_name = 'mainapp/index.html'
@@ -22,9 +24,9 @@ class HomeView(generic.TemplateView):
         count_online_drivers = 0
         user_ip = get_client_ip(request)
         orders = Order.objects.filter(user_ip=user_ip)
-        bookmark = True
-        if orders.count()>1:
-            bookmark = False
+        bookmark = False
+        if orders.count() >= 1 and orders.count() < 2:
+            bookmark = True
         active_sessions = Session.objects.filter(expire_date__gte=timezone.now())
         user_id_list = []
         for session in active_sessions:
@@ -33,7 +35,6 @@ class HomeView(generic.TemplateView):
             user_id_list.append(data.get('_auth_user_id', None))
         # Query all logged in users based on id list
         count_online_drivers = User.objects.filter(id__in=user_id_list, is_free=True).count()
-
 
         # for user in User.objects.all():
         #     if user.online() and user.is_free:
@@ -55,14 +56,15 @@ class HomeView(generic.TemplateView):
             phone_number = request.POST['phone_number']
             city = City.objects.get(name=request.POST['city'])
 
-            Order.objects.create(user_ip=user_ip, from_address=from_address, to_address=to_address, phone_number=phone_number, city=city)
+            Order.objects.create(user_ip=user_ip, from_address=from_address, to_address=to_address,
+                                 phone_number=phone_number, city=city)
         elif 'choose' in request.POST:
             offer = OfferOrder.objects.get(id=int(request.POST['offer_id']))
             driver = offer.driver_offer
             if driver.is_free == False:
                 return redirect('home_view')
             offer.is_selected = True
-            if driver.balance< offer.order.city.overpayment:
+            if driver.balance < offer.order.city.overpayment:
                 return redirect('home_view')
             driver.balance -= offer.order.city.overpayment
             driver.is_free = False
@@ -92,5 +94,6 @@ class HomeView(generic.TemplateView):
 def getMyOrders(request):
     user_ip = get_client_ip(request)
     yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-    my_orders = Order.objects.filter(user_ip=user_ip, created__gte=yesterday).exclude(status='canceled').exclude(status='finished')
+    my_orders = Order.objects.filter(user_ip=user_ip, created__gte=yesterday).exclude(status='canceled').exclude(
+        status='finished')
     return render(request, 'mainapp/ajax_my_orders.html', {'my_orders': my_orders})

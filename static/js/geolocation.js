808be0f3a3e3f63cdb0f8c51bcccbd6fe676f189
orazkarl <orazkarl>
2021-04-28 02:57:28 +0600

    change something

--- static/js/geolocation.js
+++ static/js/geolocation.js
@@ -19,6 +19,7 @@ function init() {
             balloonContentBody: 'Мое местоположение'
         });
         myMap.geoObjects.add(result.geoObjects);
+
         sendRequest(result.geoObjects.get(0).geometry.getCoordinates());
         myMap.setCenter([result.geoObjects.get(0).geometry.getCoordinates()[0], result.geoObjects.get(0).geometry.getCoordinates()[1]], 14, {checkZoomRange: true});
 
@@ -31,7 +32,7 @@ function init() {
 
 function sendRequest(state) {
     const Http = new XMLHttpRequest();
-    const url = 'https://geocode-maps.yandex.ru/1.x?geocode=' + state[1] + ',' + state[0] + '&apikey=7af4d9cc-1a22-44c4-82cf-dbd6503a6696&kind=house';
+    const url = 'https://geocode-maps.yandex.ru/1.x?geocode=' + state[1] + ',' + state[0] + '&apikey=7af4d9cc-1a22-44c4-82cf-dbd6503a6696';
     Http.open("GET", url);
     Http.send();
 
@@ -40,23 +41,41 @@ function sendRequest(state) {
         const text = data;
         const parser = new DOMParser();
         const xmlDOM = parser.parseFromString(text, "text/xml");
-        var value = text.getElementsByTagName("GeocoderMetaData")[0].getElementsByTagName("Address")[0];
-        var city = value.getElementsByTagName('Component')[2].getElementsByTagName("name")[0].childNodes[0].nodeValue;
-        var address = value.getElementsByTagName('Component')[3].getElementsByTagName("name")[0].childNodes[0].nodeValue + ',' + value.getElementsByTagName('Component')[4].getElementsByTagName("name")[0].childNodes[0].nodeValue;
-        document.querySelector('#id_cities').style.display = 'block'
-        var cities = []
-        for (var i = 0; i < document.querySelector('#id_city').options.length; i++) {
-            c = document.querySelector('#id_city').options[i]
-            if (c != undefined) {
-                cities.push(c.innerHTML)
+
+        if (text.getElementsByTagName("GeocoderMetaData")[0]) {
+            var value = text.getElementsByTagName("GeocoderMetaData")[0].getElementsByTagName("Address")[0];
+            if (value.getElementsByTagName('Component')[2]) {
+                var city = value.getElementsByTagName('Component')[2].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+                console.log(value)
+                if (value.getElementsByTagName('Component')[3]) {
+                    var address = value.getElementsByTagName('Component')[3].getElementsByTagName("name")[0].childNodes[0].nodeValue + ',' + value.getElementsByTagName('Component')[4].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+                    document.querySelector('#id_cities').style.display = 'block'
+                    var cities = []
+                    for (var i = 0; i < document.querySelector('#id_city').options.length; i++) {
+                        c = document.querySelector('#id_city').options[i]
+                        if (c != undefined) {
+                            cities.push(c.innerHTML)
+                        }
+                    }
+                    if (cities.includes(city)) {
+                        document.querySelector('#id_city').value = city
+                        document.querySelector('#id_from_address').value = address
+                    } else {
+                        document.querySelector('#error-geolocation').innerHTML = 'Город "' + city + '" не обслуживается'
+                    }
+                } else {
+                    document.querySelector('#error-geolocation').innerHTML = 'Местоположения не найдена'
+
+                }
+
+            } else {
+                document.querySelector('#error-geolocation').innerHTML = 'Местоположения не найдена'
             }
-        }
-        if (cities.includes(city)) {
-            document.querySelector('#id_city').value = city
-            document.querySelector('#id_from_address').value = address
+
         } else {
-            document.querySelector('#error-geolocation').innerHTML = 'Город "' + city + '" не обслуживается'
+            document.querySelector('#error-geolocation').innerHTML = 'Местоположения не найдена'
         }
+
 
     });
 
